e33fdded7ffbc1dae401ab0319cf78f6d4399c94
Jake <jacobtlanghel@gmail.com>
2023-03-10 23:28:12 -0500

    add images

---
+++ src/constants/images.js
@@ -0,0 +1,33 @@
+import desktopHeroImg1 from "../assets/desktop-image-hero-1.jpg";
+import desktopHeroImg2 from "../assets/desktop-image-hero-2.jpg";
+import desktopHeroImg3 from "../assets/desktop-image-hero-3.jpg";
+import mobileHeroImg1 from "../assets/mobile-image-hero-1.jpg";
+import mobileHeroImg2 from "../assets/mobile-image-hero-2.jpg";
+import mobileHeroImg3 from "../assets/mobile-image-hero-3.jpg";
+
+import aboutDarkImg from "../assets/image-about-dark.jpg";
+import aboutLightImg from "../assets/image-about-light.jpg";
+
+import angleLeft from "../assets/icons-angle-left.svg";
+import angleRight from "../assets/icons-angle-right.svg";
+import arrowIcon from "../assets/icons-arrow.svg";
+import closeIcon from "../assets/icons-close.svg";
+import hamburger from "../assets/icons-hamburger.svg";
+import logo from "../about/logo.svg";
+
+export const images = {
+  desktopHeroImg1,
+  desktopHeroImg2,
+  desktopHeroImg3,
+  mobileHeroImg1,
+  mobileHeroImg2,
+  mobileHeroImg3,
+  aboutDarkImg,
+  aboutLightImg,
+  angleLeft,
+  angleRight,
+  arrowIcon,
+  closeIcon,
+  hamburger,
+  logo,
+};
